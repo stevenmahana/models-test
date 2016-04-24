f785d52c6e4a228ef806abbe4c4e535aeaff19f1
Steven Mahana <stevenmahana@gmail.com>
2016-04-24 12:12:27 -0700

    commit

---
+++ server.py
@@ -0,0 +1,26 @@
+import time
+import zmq
+
+class Models(object):
+
+    @staticmethod
+    def receive_message():
+
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5570")
+
+        while True:
+            #  Wait for next request from client
+            message = socket.recv()
+            print("Received request: %s" % message)
+
+            #  Do some 'work'
+            time.sleep(1)
+
+            #  Send reply back to client
+            socket.send(b"Models Data")
+
+if __name__ == '__main__':
+    zs = Models()
+    zs.receive_message()
